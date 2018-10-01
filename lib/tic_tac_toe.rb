9e6eceb9d63fef347f3125a58fb18bae759407f1
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-10-01 18:29:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -72,6 +72,15 @@ class TicTacToe
      end
    end
 
-
+   def won?
+     WIN_COMBINATIONS.each do | combination |
+       if @board[combination[0]] == "X" && @board[combination[1]] == "X" && @board[combination[2]] == "X"
+         return combination # return the win_combination indexes that won.
+       elsif @board[combination[0]] == "O" && @board[combination[1]] == "O" && @board[combination[2]] == "O"
+         return combination
+       end
+     end
+        false
+   end
 
 end
